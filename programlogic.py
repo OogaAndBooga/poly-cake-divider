13988a533986f20fe9e5bbe7a54486cab7fb85fb
OogaAndBooga <79475145+OogaAndBooga@users.noreply.github.com>
2024-12-16 17:07:16 +0200

    display origin point + bugfixes + improved debug log messages

--- programlogic.py
+++ programlogic.py
@@ -1,10 +1,12 @@
 from math import dist
+from polygon import Polygon
 
 class Program_Logic():
     stage = 0
     polyline = [] # before creation of polygon
     mouse_near = False # mouse within a certain distance to polyline origin
-    origin = (0, 0)
+    origin = None #one of the defining points for division
+    origin_is_set = False
     MOUSENEARDIST = 10
 
     # points = []
@@ -19,29 +21,35 @@ class Program_Logic():
     #     self.stage += 1
 
     def set_mouse_near(self, value):
-        must_update = value != self.mouse_near
+        must_update = (value != self.mouse_near)
         self.mouse_near = value
         if must_update:
             self.draw()
 
     def mouse_move_event(self, pos):
-        if len(self.polyline) > 2 and dist(self.polyline[0], pos) < self.MOUSENEARDIST:
-            self.set_mouse_near(True)
-        else:
-            self.set_mouse_near(False)
+        if self.stage == 0:
+            if len(self.polyline) > 2 and dist(self.polyline[0], pos) < self.MOUSENEARDIST:
+                self.set_mouse_near(True)
+            else:
+                self.set_mouse_near(False)
 
     def click_event(self, pos):
         print(pos)
         if self.stage == 0:
             if not self.mouse_near:
                 self.polyline.append(pos)
-                self.draw() #function to send data to be displayed
+                 #function to send data to be displayed
             else:
                 self.stage += 1
-                self.poly = Polygon([p.toTuple() for p in self.polyline])
+                self.poly = Polygon(self.polyline)
                 print(self.polyline)
+            self.draw()
         elif self.stage == 1:
-            self.origin = pos
+            if not self.origin_is_set:
+                print(f"ORIGIN POSITION: {pos}")
+                self.origin = pos
+                self.origin_is_set = True
+                self.draw()
 
     def draw(self):
         primitives = {'points' : [],
@@ -57,8 +65,10 @@ class Program_Logic():
                 primitives['polyline'] = self.polyline
         elif self.stage == 1:
             primitives['polygon'] = self.polyline
+            if self.origin_is_set:
+                primitives['points'] += [self.origin]
 
-        print('pcalssprim', primitives)
+        print('PRIMITIVES SENT TO RENDERAREA', primitives)
         self.render_area.primitives = primitives
         self.render_area.update()
 
