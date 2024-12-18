e1af17512a80ffa6f1badf7fd79c8d0af916309d
OogaAndBooga <79475145+OogaAndBooga@users.noreply.github.com>
2024-12-18 15:15:43 +0200

    Move data structures from polygon.py to data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,124 @@
+import copy
+
+def toTuple(vec):
+    return (vec.x, vec.y)
+
+class Segment :
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.c = b - a
+
+    def ray_intersection_math(self, ray, sp = False):
+        a = self.a - ray.origin
+        b = self.b - ray.origin
+        c = self.c
+        r = ray.direction
+
+        if not sp:
+            #TODO fix the bug(slowness)
+            # r = ray.direction
+            k = (abs(b) ** 2 - c @ b - (abs(a) ** 2) * (r @ b) / (a @ r)) / ((b @ r) * (a @ c) / (a @ r) - (b @ c))
+            d = (abs(a) ** 2 + k * (a @ c)) / (a @ r)
+            solution = {'d':float(d), 'k':float(k)}
+            print(solution)
+        else:
+            #TODO divide by zero potential error
+            d, k = symbols('d, k') #d scales ray and k scales c  up to intersection point
+            eq1 = Eq(d * float(a @ ray.direction) - k * (a @ c), abs(a) ** 2)
+            eq2 = Eq(d * float(b @ ray.direction) - k * (b @ c), abs(b) ** 2 - c @ b)
+
+            solution = solve([eq1, eq2], [d, k], particular = True, quick=True)
+            print(solution)
+            solution = {'d' : solution[d], 'k' : solution[k]}
+
+        return solution
+
+class Intersection:
+    def __init__(self, point, segment):
+        self.point = point
+        self.segment = segment
+
+class Ray :
+    intersections = []
+    def __init__(self, origin, poly_vertex):
+        self.origin = origin
+        self.direction = poly_vertex - origin # vector with ray direction
+        self.poly_vertex = poly_vertex
+
+        #TODO phi or rho???
+        self.key = self.direction.phi #for sorting
+
+    def intersect_polygon(self, poly):
+        for seg in poly.segments:
+            ray_on_segment_edge = self.poly_vertex is seg.a or self.poly_vertex is seg.b
+
+            if ray_on_segment_edge:
+                self.intersections += [Intersection(self.poly_vertex, seg)]
+            else:
+                sol = seg.ray_intersection_math(self)
+                d = sol['d']
+                k = sol['k']
+
+                #TODO REMOVE d > 0 AFTER DEBUGGING
+                if d > 0 and 0 <= k <= 1:
+                    self.intersections.append( Intersection(self.origin + self.direction * d, seg))
+
+    def gen_line_tuple(self):
+        r = copy.deepcopy(self)
+        r.direction *= 100
+        r.poly_vertex = r.origin + r.direction
+        return (toTuple(r.origin), toTuple(r.poly_vertex))
+    def export(self):
+        return {
+            'origin': toTuple(self.origin),
+            'direction' : toTuple(self.direction),
+            'intersections' : [toTuple(i.point) for i in self.intersections]
+        }
+
+class Bowtie :
+    # self.rungs = None
+    def __init__(self, origin, ray1, ray2):
+        self.origin = origin
+        self.ray1 = ray1
+        self.ray2 = ray2
+
+        self.gen_ladder_rungs()
+
+        #removes intersections not in common
+        # self.remove_bad()
+
+    #assuming polyline does not intersect itself, neither will the ladder rungs
+    def gen_ladder_rungs(self):
+        self.rungs = []
+
+        r1int = self.ray1.intersections
+        r2int = self.ray2.intersections
+
+        #FIXME generate rungs
+        self.rungs = [i.segment for i in r1int]
+        # for i in range(len(r1int)):
+        #     for j in range(i + 1, len(r2int)):
+        #         if r1int[i].segment == r2int[j].segment:
+                    # self.rungs += [Segment(r1int[i].point, r2int[j].point)]
+
+        # self.rungs.sort(key = lambda a: a.)
+
+
+    #TODO assuming rays are properly sorted a segment intersecting
+    #only one of the rays intersects on one of the segment ends(k == 0 or k == 1)
+    #TODO DO NOT REMOVE ELEMENTS FROM RAYS, INSTANCES OF RAYS ARE SHARED ACROSS BOWTIES
+    def remove_bad(self):
+        r1seg = {int.seg for int in self.ray1.intersections}
+        r2seg = {int.seg for int in self.ray2.intersections}
+
+        common_segments = r1seg & r2seg
+
+        #type(amogus) = Intersection
+        for amogus in self.ray1.intersections:
+            if amogus.seg not in common_segments:
+                del amogus
+
+        for amogus in self.ray2.intersections:
+            if amogus.seg not in common_segments:
+                del amogus
